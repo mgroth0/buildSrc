8c818b4b1c4e7b1bc464c7e692fd0fead348e297
mgroth0 <mgroth49@gmail.com>
2022-05-28 14:54:22 -0400

    autocommit

--- src/main/kotlin/matt/oldkbuild/IDEbroke.kt
+++ src/main/kotlin/matt/oldkbuild/IDEbroke.kt
@@ -9,8 +9,8 @@ import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.tasks.TaskContainer
 import java.io.File
-
-@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
-inline fun <reified T: Task> TaskContainer.withType(cls: Class<T>, cfg: Action<T>) {
-  this.withType(cls).all(cfg)
-}
+//
+//@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
+//inline fun <reified T: Task> TaskContainer.withType(cls: Class<T>, cfg: Action<T>) {
+//  this.withType(cls).all(cfg)
+//}
