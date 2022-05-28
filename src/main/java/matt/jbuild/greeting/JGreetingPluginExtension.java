5dec339220f7817db86267eea32a8190385a16cd
mgroth0 <mgroth49@gmail.com>
2022-05-28 18:37:35 -0400

    autocommit

--- src/main/java/matt/jbuild/greeting/JGreetingPluginExtension.java
+++
@@ -1,7 +0,0 @@
-package matt.jbuild.greeting;
-
-public class JGreetingPluginExtension {
-    private String greeter = "Baeldung";
-    private String message = "Message from the jplugin!";
-    // standard getters and setters
-}
