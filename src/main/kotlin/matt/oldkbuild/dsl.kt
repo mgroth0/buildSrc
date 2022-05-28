123728a416152eca6eb870bbc90935af2544b2ce
mgroth0 <mgroth49@gmail.com>
2022-05-28 15:18:43 -0400

    autocommit

--- src/main/kotlin/matt/oldkbuild/dsl.kt
+++ src/main/kotlin/matt/oldkbuild/dsl.kt
@@ -5,16 +5,11 @@ so buildscripts dont need to use import statements????
 see https://github.com/gradle/gradle/issues/7557
 */
 
-import matt.klib.commons.FLOW_FOLDER
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion
 import org.gradle.api.Action
 import org.gradle.api.Project
-import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
-import org.gradle.api.tasks.Exec
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.tomlj.Toml
-import java.io.File
 
 /*SHOULD NEVER USE THIS. DOESNT WORK WITH TOOLING API*/
 /*val USER_DIR = File(System.getProperty("user.dir"))*/
